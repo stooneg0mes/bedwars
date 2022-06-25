f500f1d6c6ce18cd53b5cc4ec00676916bb1e992
stooneg0mes <stoone.holy@gmail.com>
2022-06-25 00:41:22 -0300

    feat: player interact npc listener

--- bedwars-solo/src/main/java/net/stonegomes/bedwars/command/subcommand/lobby/LobbySetNpcSubCommand.java
+++ bedwars-solo/src/main/java/net/stonegomes/bedwars/command/subcommand/lobby/LobbySetNpcSubCommand.java
@@ -5,7 +5,15 @@ import me.saiintbrisson.minecraft.command.annotation.Command;
 import me.saiintbrisson.minecraft.command.command.Context;
 import me.saiintbrisson.minecraft.command.target.CommandTarget;
 import net.stonegomes.bedwars.GamePlugin;
+import net.stonegomes.bedwars.commons.builder.ItemStackBuilder;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Giant;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 @RequiredArgsConstructor
 public class LobbySetNpcSubCommand {
@@ -19,8 +27,24 @@ public class LobbySetNpcSubCommand {
         target = CommandTarget.PLAYER
     )
     public void handleCommand(Context<Player> context) {
-        gamePlugin.getLobby().setNpcLocation(context.getSender().getLocation());
-        context.sendMessage("§aYou set the arena npc location successfully.");
+        final Player player = context.getSender();
+        player.sendMessage("§aYou set the arena npc location successfully.");
+
+        gamePlugin.getLobby().setNpcLocation(player.getLocation());
+
+        final ItemStack itemStack = new ItemStackBuilder(Material.ENDER_EYE)
+            .enchantment(Enchantment.DIG_SPEED, 1)
+            .build();
+
+        Giant giant = player.getWorld().spawn(player.getLocation(), Giant.class);
+        giant.getEquipment().setItemInMainHand(itemStack);
+        giant.getEquipment().setItemInMainHandDropChance(0.0f);
+
+        giant.setCustomNameVisible(false);
+        giant.setCanPickupItems(false);
+        giant.setRemoveWhenFarAway(false);
+
+        giant.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, 999));
     }
 
 }
